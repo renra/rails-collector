73a7dbfa5027f95b80a8f1468a53a76da729d464
Jan Renra Gloser <jan.renra.gloser@gmail.com>
2015-04-20 16:56:17 +0200

    Added http digest authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_digest do |email|
+      ::User.all[email]
+    end
+  end
 end
